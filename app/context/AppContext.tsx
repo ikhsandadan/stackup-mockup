13b9a4200363443ca65413b42ac92513707b5824
Nashki <116878888+ikhsandadan@users.noreply.github.com>
2024-07-22 05:04:51 +0700

    Update AppContext.tsx

--- app/context/AppContext.tsx
+++ app/context/AppContext.tsx
@@ -261,7 +261,7 @@ const AppProvider: FC<{ children: ReactNode }> = ({ children }) => {
             setBounty(userData.user.bounty);
             setStackcoin(userData.user.stackcoin);
             setMyOrder(userData.user.myOrder);
-            fetchAllUsersQuestsandBounty();
+            await fetchAllUsersQuestsandBounty();
 
             return userData;
         } catch (error: any) {
